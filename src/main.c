9e4e6f1f3e449717e3257209bfd7c45d9c414d88
Florian Gmeiner <florian.gmeiner@gmail.com>
2025-05-08 17:17:23 -0400

    Add starting point modbus_monitor app. NO FEATURES.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+
 #include <modbus.h>
 
-int main(void){
-
+int main(void) {
+    modbus_t *mb;
+    int connection = -1;
+    uint16_t tab_reg[32];
+  
+    mb = modbus_new_tcp("10.1.1.48", 502);
+    if (mb == NULL){
+      printf("Unable to allocate context.");
+      return -1;
+    }
+    connection = modbus_connect(mb);
+    if (connection < 0) {
+      printf("Failed to connect.\n");
+      return -1;
+    }
+    printf("Connection established\n");
+  
+    /* Read 5 registers from the address 0 */
+    modbus_read_registers(mb, 0, 5, tab_reg);
+  
+    modbus_close(mb);
+    modbus_free(mb);
+    
     return 0;
-}
+  }
